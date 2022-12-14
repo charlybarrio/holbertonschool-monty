f736abc3eed08ea279ac6d5dea89b7a2cc1f61da
jgnacio <jgnaciogomez@gmail.com>
2022-12-14 18:06:02 -0300

    add implementation for options functions verify

--- test-monty.c
+++ test-monty.c
@@ -6,32 +6,45 @@
 int main (__attribute__((unused)) int argc, char **argv)
 {
 	FILE *fp = NULL;
-	char *line = NULL;
-	char **split_line = NULL;
-	int index;
+	char *line = NULL, **split_line = NULL;
+	int line_number; /* variable for read line number */
 	size_t len = 0;
 	ssize_t read;
+	void (*fun)(); /* variable for get the option function */
+	stack_t *stack = NULL; /* double linked list (not implemented)*/
 
-	if (argc < 1)
+	if (argc != 2)
 	{
 		printf("USAGE: monty file");
-		return (1);
+		exit(EXIT_FAILURE);
 	}
 
-	/* read the file 00.m */
+	/* open the file with read permissions*/
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 		exit(EXIT_FAILURE);
 
-	while ((read = getline(&line, &len, fp)) != -1)
+	/* read the file line per line */
+	for (line_number = 1; (read = getline(&line, &len, fp)) != -1; line_number++)
 	{
-		printf("Retrieved line of length %ld:\n", read);
-		printf("%s", line);
+		/* tokenize current line */
 		split_line = split(line, " \t\n");
-		printf("-- TOKENIZE-STR --\n");
-		for (index = 0; split_line[index]; index++)
-			printf("%s\n", split_line[index]);
-		printf("-- TOKENIZE-END --\n");
+		if (!split_line) /* check tokenize and print error if it fails */
+		{
+			dprintf(STDERR_FILENO, "Error: malloc failed");
+			exit(EXIT_FAILURE);
+		}
+		fun = op_fun(split_line); /* get the option function */
+		if (!fun) /* check function return and print error if it fails*/
+		{
+			dprintf(STDERR_FILENO, "L%d: unknown instruction\n", line_number);
+			fclose(fp);
+			if (line) /* free memory of getline */
+				free(line), line = NULL;
+			exit(EXIT_FAILURE);
+		}
+		fun (&stack, line_number); /* execute the function return */
+		/* free memory and set to NULL for nexts iterations */
 		free(split_line), split_line = NULL;
 	}
 	fclose(fp);
@@ -40,3 +53,14 @@ int main (__attribute__((unused)) int argc, char **argv)
 	exit(EXIT_SUCCESS);
 }
 
+void push(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	printf("push-fun\n");
+}
+void pall(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	printf("pall-fun\n");
+}
+void printerror()
+{
+}
