981510d259a2311319848c1c5c885387dc52742f
jgnacio <jgnaciogomez@gmail.com>
2022-12-18 19:56:16 -0300

    fix stderr output

--- options.c
+++ options.c
@@ -47,7 +47,7 @@ void pop(stack_t **head, unsigned int line_number)
 
 	if (!head || !(*head))
 	{
-		dprintf(STDOUT_FILENO, "L%d: can't pop an empty stack\n", line_number);
+		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
 		buf_tok = NULL;
 		return;
 	}
