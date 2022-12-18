10fef975bfe084821f4ae5048ca94766ef732924
jgnacio <jgnaciogomez@gmail.com>
2022-12-18 10:24:49 -0300

    update options functions

---
+++ options.c
@@ -0,0 +1,94 @@
+#include "main.h"
+
+/**
+ * push - function thath prints the value at the top of the stack,
+ * followed by a new line
+ * @stack: is a pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	int tok_int = 0;
+
+	buf_tok = strtok(NULL, DELIM);
+	if (!is_number(buf_tok))
+	{
+		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
+		buf_tok = NULL;
+		return;
+	}
+	tok_int = atoi(buf_tok);
+	add_tostack(stack, tok_int);
+}
+
+/**
+ * pall - function thath prints the value at the top of the stack,
+ * followed by a new line
+ * @stack: is a pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+	print_stack(*stack);
+}
+
+/**
+ * pop - Deletes a node
+ * @head: Pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void pop(stack_t **head, unsigned int line_number)
+{
+	stack_t *node = NULL;
+
+	(void)line_number;
+	if (!head || !(*head))
+		return;
+	node = *head;
+	*head = (*head)->next;
+	if (*head)
+		(*head)->prev = NULL;
+	free(node);
+}
+
+/**
+ * pint - function thath prints the value at the top of the stack,
+ * followed by a new line
+ * @head: is a pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void pint(stack_t **head, unsigned int line_number)
+{
+	if (!head || !(*head))
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+		buf_tok = NULL;
+		return;
+	}
+	dprintf(STDOUT_FILENO, "%d\n", (*head)->n);
+}
+
+/**
+ * swap - function that swaps the top two elements of the stack
+ * @head: is a pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void swap(stack_t **head, unsigned int line_number)
+{
+	stack_t *tmp = *head;
+	int len, n;
+
+	for (len = 1; (tmp && (tmp)->next) && len < 2; len++)
+		tmp = tmp->next;
+	if (len < 2)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n", line_number);
+		buf_tok = NULL;
+		return;
+	}
+
+	n = (*head)->n;
+	(*head)->n = ((*head)->next)->n;
+	((*head)->next)->n = n;
+}
---
+++ options2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * add - function that swaps the top two elements of the stack
+ * @head: is a pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void add(stack_t **head, unsigned int line_number)
+{
+	stack_t *tmp = *head;
+	int len, result;
+
+	for (len = 1; (head && (tmp)->next) && len < 2; len++)
+		tmp = tmp->next;
+	if (len < 2)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line_number);
+		buf_tok = NULL;
+		return;
+	}
+
+	result = (*head)->n + ((*head)->next)->n;
+	((*head)->next)->n = result;
+	((*head)->next)->prev = NULL;
+	pop(&(*head), line_number);
+}
+
+/**
+ * nop - function that doesn’t do anything
+ * @head: is a pointer to the top of the stack
+ * @line_number: the current line read
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
